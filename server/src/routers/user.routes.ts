332078ecca160c5911553d6e3173949bf2a9cdb8
rhizo <birdsarentreal17@gmail.com>
2024-02-05 14:10:47 -0600

    create user router, model, and controller files

---
+++ server/src/routers/user.routes.ts
@@ -0,0 +1,11 @@
+import express from "express";
+
+import {
+
+} from "../controllers/user.controller";
+
+const userRouter = express.Router();
+
+/* http requests here */
+
+export default userRouter;
