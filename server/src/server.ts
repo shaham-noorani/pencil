4c665a4c213f9ad6f46481495456c02c579b4582
rhizo <birdsarentreal17@gmail.com>
2024-02-05 15:22:17 -0600

    Fix bug to mount middleware to api/ path

--- server/src/server.ts
+++ server/src/server.ts
@@ -7,7 +7,7 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
-app.use(router);
+app.use("/api", router);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
