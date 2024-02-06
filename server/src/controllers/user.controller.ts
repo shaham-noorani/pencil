8701124bf3b4d265f0e781515efda165e1931965
rhizo <birdsarentreal17@gmail.com>
2024-02-06 15:19:40 -0600

    Delete user function complete

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -80,7 +80,20 @@ export const updateUserEmail = async (req: Request, res: Response) => {
 
 //Delete Users
 export const deleteUser = async (req: Request, res: Response) => {
-    return res.status(501).json({ message: "Not Implemented" });
+    try {
+        const result = await pool.query(
+          "DELETE FROM users WHERE id = $1 RETURNING *",
+          [req.params.id]
+        );
+        const user = result.rows[0];
+    
+        if (!user) {
+          return res.status(404).json({ message: "User not found" });
+        }
+        res.status(200).json({ message: "User deleted successfully" });
+      } catch (error: any) {
+        res.status(500).json({ message: error.message });
+      }
 };
 
 //Create Users
