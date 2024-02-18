0ada0e6f2ddf2d6b66b6c3db443cc35c44efb814
conrad-krueger <conradjkrueger@gmail.com>
2024-02-18 15:43:45 -0600

    Net worth table http methods

---
+++ server/src/controllers/netWorth.controller.ts
@@ -0,0 +1,104 @@
+import { Request, Response } from "express";
+import { pool } from "../db";
+
+//Get PlaidItems
+export const getAllNetWorths = async (req: Request, res: Response) => {
+  try {
+    const result = await pool.query("SELECT * FROM user_net_worth");
+    const nws = result.rows;
+    res.status(200).json(nws);
+  } catch (error: any) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const getNetWorthById = async (req: Request, res: Response) => {
+  try {
+    const result = await pool.query(
+      "SELECT * FROM user_net_worth WHERE id = $1",
+      [req.params.id]
+    );
+    const nw = result.rows[0];
+    if (!nw) {
+      return res.status(404).json({ message: "User Net Worth not found" });
+    }
+    res.status(200).json(nw);
+  } catch (error: any) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const getNetWorthsByUserId = async (req: Request, res: Response) => {
+  try {
+    const result = await pool.query(
+      "SELECT * FROM user_net_worth WHERE user_id = $1",
+      [req.params.user_id]
+    );
+    const nw = result.rows[0];
+    if (!nw) {
+      return res.status(404).json({ message: "User Net Worth not found" });
+    }
+    res.status(200).json(nw);
+  } catch (error: any) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+//Delete Spendings
+export const deleteNetWorth = async (req: Request, res: Response) => {
+  try {
+    const result = await pool.query(
+      "DELETE FROM user_net_worth WHERE id = $1 RETURNING *",
+      [req.params.id]
+    );
+    const nw = result.rows[0];
+
+    if (!nw) {
+      return res.status(404).json({ message: "User Net Worth not found" });
+    }
+    res.status(200).json({ message: "User Net Worth deleted successfully" });
+  } catch (error: any) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const deleteNetWorthByUserId = async (req: Request, res: Response) => {
+  try {
+    const result = await pool.query(
+      "DELETE FROM user_net_worth WHERE user_id = $1 RETURNING *",
+      [req.params.user_id]
+    );
+    const nw = result.rows[0];
+
+    if (!nw) {
+      return res.status(404).json({ message: "User Net Worth not found" });
+    }
+    res.status(200).json({ message: "User Net Worth deleted successfully" });
+  } catch (error: any) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+//Create Spendings
+export const createNetWorth = async (req: Request, res: Response) => {
+  try {
+    let start_date = req.body.start_date;
+    let end_date = req.body.end_date;
+
+    //Assume spending is money spent from 7 days ago until now
+    if (req.body.start_date === undefined || req.body.end_date === undefined) {
+      end_date = new Date();
+      start_date = new Date(end_date.getTime() - 1000 * 60 * 60 * 24 * 7);
+    }
+
+    const result = await pool.query(
+      "INSERT INTO user_net_worth (start_date, end_date, amount, user_id) VALUES ($1, $2, $3, $4) RETURNING *",
+      [start_date, end_date, req.body.amount, req.body.user_id]
+    );
+    const nw = result.rows[0];
+
+    res.status(201).json(nw);
+  } catch (error: any) {
+    res.status(400).json({ message: error.message });
+  }
+};
