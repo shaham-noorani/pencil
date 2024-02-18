a0d99dc423257ecfe315fa88436ccc79fd1ec665
conrad-krueger <conradjkrueger@gmail.com>
2024-02-18 02:36:30 -0600

    Plaid Item controller implementation

---
+++ server/src/controllers/plaidItem.controller.ts
@@ -0,0 +1,94 @@
+import { Request, Response } from "express";
+import { pool } from "../db";
+
+//Get PlaidItems
+export const getAllPlaidItems = async (req: Request, res: Response) => {
+  try {
+    const result = await pool.query("SELECT * FROM plaid_item");
+    const users = result.rows;
+    res.status(200).json(users);
+  } catch (error: any) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const getPlaidItemById = async (req: Request, res: Response) => {
+  try {
+    const result = await pool.query("SELECT * FROM plaid_item WHERE id = $1", [
+      req.params.id,
+    ]);
+    const user = result.rows[0];
+    if (!user) {
+      return res.status(404).json({ message: "Plaid Item not found" });
+    }
+    res.status(200).json(user);
+  } catch (error: any) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const getPlaidItemsByUserId = async (req: Request, res: Response) => {
+  try {
+    const result = await pool.query(
+      "SELECT * FROM plaid_item WHERE user_id = $1",
+      [req.params.user_id]
+    );
+    const user = result.rows[0];
+    if (!user) {
+      return res.status(404).json({ message: "Plaid Items not found" });
+    }
+    res.status(200).json(user);
+  } catch (error: any) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+//Delete Users
+export const deletePlaidItem = async (req: Request, res: Response) => {
+  try {
+    const result = await pool.query(
+      "DELETE FROM plaid_item WHERE id = $1 RETURNING *",
+      [req.params.id]
+    );
+    const user = result.rows[0];
+
+    if (!user) {
+      return res.status(404).json({ message: "Plaid Item not found" });
+    }
+    res.status(200).json({ message: "Plaid Item deleted successfully" });
+  } catch (error: any) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const deletePlaidItemByUserId = async (req: Request, res: Response) => {
+  try {
+    const result = await pool.query(
+      "DELETE FROM plaid_item WHERE user_id = $1 RETURNING *",
+      [req.params.user_id]
+    );
+    const user = result.rows[0];
+
+    if (!user) {
+      return res.status(404).json({ message: "Plaid Item not found" });
+    }
+    res.status(200).json({ message: "Plaid Item deleted successfully" });
+  } catch (error: any) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+//Create Users
+export const createPlaidItem = async (req: Request, res: Response) => {
+  try {
+    const result = await pool.query(
+      "INSERT INTO plaid_item (token, user_id) VALUES ($1, $2) RETURNING *",
+      [req.body.token, req.body.user_id]
+    );
+    const user = result.rows[0];
+
+    res.status(201).json(user);
+  } catch (error: any) {
+    res.status(400).json({ message: error.message });
+  }
+};
