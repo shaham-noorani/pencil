5513f4acb3aeea885b5286ab4b8fb27a98bf05c2
conrad-krueger <conradjkrueger@gmail.com>
2024-02-20 16:35:41 -0600

    Include middleware

--- server/src/server.ts
+++ server/src/server.ts
@@ -11,9 +11,8 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
-//authMiddleware,
-app.use("/api", router);
-//app.use("/auth", authRouter);
+app.use("/api", authMiddleware, router);
+app.use("/auth", authRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
