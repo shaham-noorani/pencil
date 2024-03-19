c00c792134a38a707cfc78fa189bf5e9090bf99e
m-argarita-m <anirudh.margam@gmail.com>
2024-03-19 13:40:22 -0500

    deleted misplaced burn rate module files and added plus button to dashboard header to navigate user to connect account page

--- client/src/modules/dashboard/HeaderNetWorth.tsx
+++ client/src/modules/dashboard/HeaderNetWorth.tsx
@@ -1,8 +1,11 @@
-import { Flex, Text } from "@chakra-ui/react";
+import { Button, Flex, Text } from "@chakra-ui/react";
+import { AddIcon } from "@chakra-ui/icons";
 import { format } from "date-fns";
+import { useNavigate } from "react-router-dom";
 
 const HeaderNetWorth = () => {
   const today = format(new Date(), "EEEE, MMMM do");
+  const navigate = useNavigate();
 
   return (
     <Flex
@@ -14,9 +17,17 @@ const HeaderNetWorth = () => {
       alignItems="flex-start"
       marginTop="5%"
     >
-      <Text fontSize="3xl" fontWeight="bold">
-        Home
-      </Text>
+      <Flex w="full" justifyContent="space-between" alignItems="center">
+        <Text fontSize="3xl" fontWeight="bold">
+          Home
+        </Text>
+        <Button
+          leftIcon={<AddIcon color="white" />}
+          onClick={() => navigate("/connect-account")}
+          fontSize={"sm"}
+          bg="#222222"
+        />
+      </Flex>
       <Text fontSize="md">{today}</Text>
     </Flex>
   );
