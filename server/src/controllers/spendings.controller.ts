12138d337ade068d4b14e53948133af43a605fd9
conrad-krueger <conradjkrueger@gmail.com>
2024-04-01 11:00:49 -0500

    Fun budget calc

--- server/src/controllers/spendings.controller.ts
+++ server/src/controllers/spendings.controller.ts
@@ -20,7 +20,7 @@ export const getSpendingsById = async (req: Request, res: Response) => {
   try {
     const result = await pool.query(
       "SELECT * FROM user_spendings WHERE id = $1",
-      [req.params.id],
+      [req.params.id]
     );
     const spent = result.rows[0];
     if (!spent) {
@@ -36,7 +36,7 @@ export const getSpendingsByUserId = async (req: Request, res: Response) => {
   try {
     const result = await pool.query(
       "SELECT * FROM user_spendings WHERE user_id = $1",
-      [req.params.user_id],
+      [req.params.user_id]
     );
     const spent = result.rows[0];
     if (!spent) {
@@ -48,21 +48,24 @@ export const getSpendingsByUserId = async (req: Request, res: Response) => {
   }
 };
 
-export const getSpendingsByUserIdAndDateRange = async (req: Request, res: Response) => {
+export const getSpendingsByUserIdAndDateRange = async (
+  req: Request,
+  res: Response
+) => {
   try {
     const result = await pool.query(
       "SELECT * FROM user_spendings WHERE user_id = $1 AND end_date >= $2 AND start_date <= $3 ORDER BY start_date",
-      [req.params.user_id, req.query.start_date, req.query.end_date],
+      [req.params.user_id, req.query.start_date, req.query.end_date]
     );
     const spent = result.rows;
 
-    if (!spent){
-      return res.status(404).json({message: "User Spendings not found"})
+    if (!spent) {
+      return res.status(404).json({ message: "User Spendings not found" });
     }
 
     res.status(200).json(result);
-  } catch (error: any){
-    res.status(500).json({message: error.message});
+  } catch (error: any) {
+    res.status(500).json({ message: error.message });
   }
 };
 
@@ -71,7 +74,7 @@ export const deleteSpendings = async (req: Request, res: Response) => {
   try {
     const result = await pool.query(
       "DELETE FROM user_spendings WHERE id = $1 RETURNING *",
-      [req.params.id],
+      [req.params.id]
     );
     const spent = result.rows[0];
 
@@ -88,7 +91,7 @@ export const deleteSpendingsByUserId = async (req: Request, res: Response) => {
   try {
     const result = await pool.query(
       "DELETE FROM user_spendings WHERE user_id = $1 RETURNING *",
-      [req.params.user_id],
+      [req.params.user_id]
     );
     const spent = result.rows[0];
 
@@ -111,7 +114,58 @@ export const createSpendingsController = async (
     const lr = await buildLinearRegression(req.body.user_id);
 
     res.status(201).json(spending);
+  } catch (error: any) {
+    res.status(400).json({ message: error.message });
+  }
+};
 
+export const getFunBudget = async (req: Request, res: Response) => {
+  try {
+    //Get curr month
+    let date = new Date();
+    let month = date.getMonth();
+    let year = date.getFullYear();
+    const AUGUST = 7;
+    const milliseconds_to_days = 1000 * 60 * 60 * 24;
+
+    let beginMonth = new Date(year, month, 1);
+
+    if (month < AUGUST) {
+      year -= 1;
+    }
+
+    let diff = beginMonth.getTime() - new Date(year, AUGUST, 1).getTime();
+    diff = Math.ceil(diff / milliseconds_to_days);
+
+    //Get current amount
+    let curr_amount = 10000; //FIX
+    let starting_amount = curr_amount;
+    let last_month = curr_amount;
+
+    //Calculate Starting Amount
+    const result = await pool.query(
+      "SELECT * FROM user_spendings WHERE user_id = $1 AND end_date >= $2 AND start_date <= $3 ORDER BY start_date",
+      [req.params.user_id, req.query.start_date, req.query.end_date]
+    );
+    const spent = result.rows;
+
+    for (let i = 0; i < spent.length; i++) {
+      starting_amount += spent[i]["spent_amount"];
+      if (new Date(spent[i]["end_date"]).getMonth() == month) {
+        last_month += spent[i]["spent_amount"];
+      }
+    }
+
+    //Get Goal Budget
+    const result_burn_rate = await pool.query(
+      "SELECT burn_rate_goal FROM users WHERE user_id = $1",
+      [req.params.user_id]
+    );
+    const brg = result_burn_rate.rows[0]["burn_rate_goal"];
+
+    const desired_amount = ((starting_amount - brg) / 10) * month;
+
+    res.status(201).json({ fun_budget: last_month - desired_amount });
   } catch (error: any) {
     res.status(400).json({ message: error.message });
   }
