e4c69a316fdc1afe31f1d4955186daf6170773ca
m-argarita-m <anirudh.margam@gmail.com>
2024-04-16 14:32:54 -0500

    fixed tooltip on net worth line chart

--- client/src/modules/dashboard/LinechartNetWorth.tsx
+++ client/src/modules/dashboard/LinechartNetWorth.tsx
@@ -22,13 +22,27 @@ const LinechartNetWorth = ({
     maxNetWorth + maxNetWorthDifference * 0.1,
   ];
 
+  console.log("\n\n\tNETWORTH DATA\n\n");
+  console.log(data);
+  console.log("\n\n\tNETWORTH DATA\n\n");
+
   return (
     <Box width="92%" margin="0 auto">
       <ResponsiveContainer width="100%" height={200}>
         <LineChart data={data} margin={{ right: 10, left: 10 }}>
-          <XAxis dataKey="name" tick={{ fill: "white", fontWeight: "bold" }} />
+          <XAxis
+            dataKey="name"
+            tickFormatter={(value) => value[0]}
+            tick={{ fill: "white", fontWeight: "bold" }}
+          />
           <YAxis domain={yAxisDomain} hide={true} />
-          <Tooltip />
+          <Tooltip
+            formatter={(value, name) => {
+              const formattedValue =
+                value as number < 0 ? `-$${Math.abs(value as number)}` : `$${value}`;
+              return [formattedValue, "Net Worth"];
+            }}
+          />{" "}
           <Line
             type="monotone"
             dataKey="value"
